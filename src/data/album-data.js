d48996243ee91c2f4aff48216c13deb69c891c8e
Hashir <70011762+HashMaster02@users.noreply.github.com>
2025-10-28 17:58:09 -0500

    Created a data directory which contains sitewide data for albums (and eventually singles)

---
+++ src/data/album-data.js
@@ -0,0 +1,9 @@
+export const albums = [
+  {
+    title: "Learners Toolkit Deluxe Edition",
+    year: "2022",
+    type: "album",
+    slug: "learners-toolkit-deluxe",
+    cover: "learners_toolkit_deluxe.jpg",
+  },
+];
