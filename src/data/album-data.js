d50b68024ab038063826c269fe7f7348e0889382
Hashir <70011762+HashMaster02@users.noreply.github.com>
2025-10-31 16:41:32 -0500

    Updated (temporary) data for Learner's Toolkit album

--- src/data/album-data.js
+++ src/data/album-data.js
@@ -1,9 +1,10 @@
 export const albums = [
   {
-    title: "Learners Toolkit Deluxe Edition",
-    year: "2022",
+    title: "Learner's Toolkit",
+    year: "2023",
     type: "album",
-    slug: "learners-toolkit-deluxe",
-    cover: "learners_toolkit_deluxe.jpg",
+    slug: "learners-toolkit",
+    cover: "/blog/learners-toolkit/learners_toolkit_cover.jpg",
+    blurb: "This is the blurb for Learners Toolkit...",
   },
 ];
